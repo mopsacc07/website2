5973228090646799b9a9826e214526759e80f8e3
elmehdi waddad <ewaddad@cofarma.co.ma>
2025-04-10 14:05:04 +0100

    commit

---
+++ src/main/java/marjane/wem/WemApplication.java
@@ -0,0 +1,13 @@
+package marjane.wem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marjane/wem/WemApplicationTests.java
@@ -0,0 +1,13 @@
+package marjane.wem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
